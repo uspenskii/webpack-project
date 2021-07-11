a5b86b687bd75dfe080e795e5c04145a77a56197
uspenskii <adndrei.uspenskii@gmail.com>
2021-07-11 16:53:16 +0300

    Webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,20 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js", //Точка входа откуда берутся все файлы
-  devtool: "eval-source-map", //Инструменты для разрабочика, отладки кода
+  entry: "./src/index.js",
+  devtool: "eval-source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js", //имя файла сборки
+    filename: "bundle.js", 
   },
   module: {
     rules: [
       {
-        test: /\.m?js$/, //Влияет на то файл какого расширения мы возьмем
-        exclude: /(node_modules|bower_components)/, // Файлы которые не включать в конвертацию
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/, 
         use: {
-          //Указывает вебпаку какой лоудер использовать для данных типов файлов
           loader: "babel-loader",
           options: {
-            //дополнительные настройки
             presets: ["@babel/preset-env"],
           },
         },
